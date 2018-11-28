42b5c807be555e19d32cf40afe504c8394c9277a
emirdal1020 <45396268+emirdal1020@users.noreply.github.com>
2018-11-28 20:46:01 +0300

    Update ksfdmkm.js

--- komutlar/ksfdmkm.js
+++ komutlar/ksfdmkm.js
@@ -1,43 +1,11 @@
-const Discord = require('discord.js');
-
-exports.run = (client, message, args) => {
-
-    if (!message.guild) {
-    const ozelmesajuyari = new Discord.RichEmbed()
-    .setColor(0x2488E7)
-    .setTimestamp()
-    .setAuthor(message.author.username, message.author.avatarURL)
-    .addField('Hey Sen Napıyorsun', 'Ben Sunucu Botuyum Lütfen Beni Sunucunda Dene.')
-    return message.author.sendEmbed(ozelmesajuyari); }
-
-  let mesaj = args.slice(0).join(' ');
-if (mesaj.length < 1) return message.channel.send('Birşey Yazmalısınız');
-
-  message.delete();
-
-  console.log(`Duyuru: "${message.author.username}#${message.author.discriminator}" "${mesaj}"`);
-
-      const mesajat = new Discord.RichEmbed()
-      .setColor('RANDOM')
-      .setDescription('' + mesaj + '')
-
-      client.users.forEach(u => {
-u.sendEmbed(mesajat)
-})
-
-message.channel.send(`:white_check_mark: Mesaj basariyla **` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + `** kullanıcıya gonderildi.`);
-
-};
-
-exports.conf = {
-  enabled: true,
-  guildOnly: true,
-  aliases: ['duyurla'],
-  permLevel: 4
-};
-
-exports.help = {
-  name: 'duyur',
-  description: 'İstediğiniz şeyi bota duyurtur.',
-  usage: 'duyuru [duyurmak istediğiniz şey]'
-};
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find('name', 'gelen-giden');
+  if (!channel) return;
+  const embed = new Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setAuthor(member.user.username, member.user.avatarURL)
+  .setThumbnail(member.user.avatarURL)
+  .setTitle('📤 | Sunucuya Katıldı | Hoşgeldin ')
+  .setTimestamp()
+  channel.sendEmbed(embed);
+});
