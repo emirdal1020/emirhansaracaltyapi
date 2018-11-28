9d5432a4a307b6d3463cc72220e631c76c6b3b7c
emirdal1020 <45396268+emirdal1020@users.noreply.github.com>
2018-11-28 15:30:28 +0300

    Add files via upload

---
+++ komutlar/sahip.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let serverembed = new Discord.RichEmbed()
+    .setDescription("**:chicken: Sahip komutları**")
+    .setColor("#15f153")
+    .addField("+eval", "Kod dener")
+    .addField("+load", "Yeni bir komut yükler")
+    .addField("+reboot", "Botu yeniden başlatır")
+    .addField("+reload", "Bir komutu yeniden başlatır")
+    .addField("+unload", "Bir komutu devre dışı bırakır")
+
+    message.channel.send(serverembed);
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['sahipler'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'sahip',
+  description: 'Bütün sahip komutlarını verir.',
+  usage: 'sahip'
+};
