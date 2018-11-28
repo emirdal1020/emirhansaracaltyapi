d5145419193c8275f5dc90d81bfdaad9fa5c4172
emirdal1020 <45396268+emirdal1020@users.noreply.github.com>
2018-11-28 17:46:24 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,1 +1,28 @@
+bot.on('guildMemberAdd', member => {
+  let guild = member.guild;
+  let joinRole = guild.roles.find('name', 'Üye');//
+  member.sendMessage("Sunucuya Hoşgeldin Kardeşim İnşallah eğlenirsin sunucumuzda.")
+  member.addRole(joinRole);
 
+  const channel = member.guild.channels.find('name', 'gelen-giden');
+  if (!channel) return;
+  const embed = new Discord.RichEmbed()
+  .setColor('0x00cc44')
+  .setAuthor(bot.user.username, bot.user.avatarURL)
+  .setThumbnail(member.user.avatarURL)
+  .setTitle(`:inbox_tray: ${member.user.username} Sunucuya Hoşgeldin Kanka`)
+  .setTimestamp()
+  channel.sendEmbed(embed);
+});
+
+bot.on('guildMemberRemove', member => {
+  const channel = member.guild.channels.find('name', 'gelen-giden');//
+  if (!channel) return;
+  const embed = new Discord.RichEmbed()
+  .setColor('0xff1a1a')
+  .setAuthor(bot.user.username, bot.user.avatarURL)
+  .setThumbnail(member.user.avatarURL)
+  .setTitle(`:outbox_tray: ${member.user.username} Açaydım Kollarımı Gitme Diyeydim!`)
+  .setTimestamp()
+  channel.sendEmbed(embed);
+});
