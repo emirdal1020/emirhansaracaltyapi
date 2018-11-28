5da053dca1c41763610777fd6f91fc5edc63a312
emirdal1020 <45396268+emirdal1020@users.noreply.github.com>
2018-11-28 21:43:45 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,7 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("dnd");
-  client.user.setGame(`Bot Tekrar Aktif! | Botumuz Hacklenmisti Geri Kurtarmayi Basardik.`);
+  client.user.setGame(`Bot Tekrar Aktif! | Reklam Koruması Getirildi Stres Çarkı Geliştirildi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
