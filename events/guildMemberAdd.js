39451ef4da1ec762a8bc248ed7ccf4b400efb5ae
emirdal1020 <45396268+emirdal1020@users.noreply.github.com>
2018-11-28 17:43:59 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,1 @@
-module.exports = member => {
-    let username = member.user.username;
-    member.send('Hoş geldin **' + username + '**!');
-    member.guild.defaultChannel.send('hg '+username+'');
-};
+
