341a3c97bbf6d0b25b973e7e38a971dd2f9ff982
emirdal1020 <45396268+emirdal1020@users.noreply.github.com>
2018-11-28 15:45:23 +0300

    Add files via upload

---
+++ komutlar/sdgs.js
@@ -0,0 +1,38 @@
+﻿const Discord = require('discord.js');
+
+
+
+exports.run = async (client, message) => {
+    let dönme = await message.channel.send({
+        embed: {
+            color: 0x00AE86,
+            description: `${message.author.tag} Adli Reyiz bir stres çarkı çevirdi!`,
+            image: {
+                url: "http://c10.haberself.com/res/haberself/59/4/261154_o5c20.gif"
+            }
+        }
+    });
+
+    let bitiş = (Math.random() * (65 - 5 +1)) + 5;
+    setTimeout(() => {
+        dönme.edit({
+            embed: {
+                color: 0x00AE86,
+                description: `${message.author.tag}, Kanka stres çarkın ${bitiş.toFixed(2)} saniye döndü.`
+            }
+        });
+    }, 5 * 1000);
+};  
+
+exports.conf = {
+  enabled: true, 
+  guildOnly: false, 
+  aliases: [],
+  permLevel: 0 
+};
+
+exports.help = {
+  name: 'stresçarkı', 
+  description: 'Stres çarkınla meydan oku!',
+  usage: 'stresçarkı'
+};
